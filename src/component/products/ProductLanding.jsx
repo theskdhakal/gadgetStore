a7fea6878456f923eba17741d6f1bed248e44d94
Shiva Dhakal <theskdhakal@gmail.com>
2023-08-11 18:59:57 +1000

    product landing page assoicated with specific product

--- src/component/products/ProductLanding.jsx
+++ src/component/products/ProductLanding.jsx
@@ -2,50 +2,52 @@ import { useState } from "react";
 import { StarIcon } from "@heroicons/react/20/solid";
 import { RadioGroup } from "@headlessui/react";
 import { MainLayout } from "../layout/main-layout/MainLayout";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-const product = {
-  name: "Basic Tee 6-Pack",
-  price: "$192",
-  href: "#",
-  breadcrumbs: [
-    { id: 1, name: "Men", href: "#" },
-    { id: 2, name: "Clothing", href: "#" },
-  ],
-  images: [
-    {
-      src: "https://tailwindui.com/img/ecommerce-images/product-page-02-secondary-product-shot.jpg",
-      alt: "Two each of gray, white, and black shirts laying flat.",
-    },
-    {
-      src: "https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-01.jpg",
-      alt: "Model wearing plain black basic tee.",
-    },
-    {
-      src: "https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-02.jpg",
-      alt: "Model wearing plain gray basic tee.",
-    },
-    {
-      src: "https://tailwindui.com/img/ecommerce-images/product-page-02-featured-product-shot.jpg",
-      alt: "Model wearing plain white basic tee.",
-    },
-  ],
-  colors: [
-    { name: "White", class: "bg-white", selectedClass: "ring-gray-400" },
-    { name: "Gray", class: "bg-gray-200", selectedClass: "ring-gray-400" },
-    { name: "Black", class: "bg-gray-900", selectedClass: "ring-gray-900" },
-  ],
+// const product = {
+//   name: "Basic Tee 6-Pack",
+//   price: "$192",
+//   href: "#",
+//   breadcrumbs: [
+//     { id: 1, name: "Men", href: "#" },
+//     { id: 2, name: "Clothing", href: "#" },
+//   ],
+//   images: [
+//     {
+//       src: "https://tailwindui.com/img/ecommerce-images/product-page-02-secondary-product-shot.jpg",
+//       alt: "Two each of gray, white, and black shirts laying flat.",
+//     },
+//     {
+//       src: "https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-01.jpg",
+//       alt: "Model wearing plain black basic tee.",
+//     },
+//     {
+//       src: "https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-02.jpg",
+//       alt: "Model wearing plain gray basic tee.",
+//     },
+//     {
+//       src: "https://tailwindui.com/img/ecommerce-images/product-page-02-featured-product-shot.jpg",
+//       alt: "Model wearing plain white basic tee.",
+//     },
+//   ],
+//   colors: [
+//     { name: "White", class: "bg-white", selectedClass: "ring-gray-400" },
+//     { name: "Gray", class: "bg-gray-200", selectedClass: "ring-gray-400" },
+//     { name: "Black", class: "bg-gray-900", selectedClass: "ring-gray-900" },
+//   ],
 
-  description:
-    'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
-  highlights: [
-    "Hand cut and sewn locally",
-    "Dyed with our proprietary colors",
-    "Pre-washed & pre-shrunk",
-    "Ultra-soft 100% cotton",
-  ],
-  details:
-    'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
-};
+//   description:
+//     'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
+//   highlights: [
+//     "Hand cut and sewn locally",
+//     "Dyed with our proprietary colors",
+//     "Pre-washed & pre-shrunk",
+//     "Ultra-soft 100% cotton",
+//   ],
+//   details:
+//     'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
+// };
 const reviews = { href: "#", average: 4, totalCount: 117 };
 
 function classNames(...classes) {
@@ -53,47 +55,33 @@ function classNames(...classes) {
 }
 
 export const ProductLanding = () => {
-  const [selectedColor, setSelectedColor] = useState(product.colors[0]);
+  const { slug } = useParams();
+  console.log(slug);
+
+  const { product } = useSelector((state) => state.product);
+  const selectedProduct = product.find((item) => item.slug === slug) || {};
+  console.log(selectedProduct);
+
+  const { productName, price, thumbnail, salesPrice, description } =
+    selectedProduct;
+  //   const [selectedColor, setSelectedColor] = useState(product.colors[0]);
 
   return (
     <MainLayout>
       <div className="bg-white">
-        <div className="pt-6">
+        <div key={slug} className="pt-6">
           <nav aria-label="Breadcrumb">
             <ol
               role="list"
               className="mx-auto flex max-w-2xl items-center space-x-2 px-4 sm:px-6 lg:max-w-7xl lg:px-8"
             >
-              {product.breadcrumbs.map((breadcrumb) => (
-                <li key={breadcrumb.id}>
-                  <div className="flex items-center">
-                    <a
-                      href={breadcrumb.href}
-                      className="mr-2 text-sm font-medium text-gray-900"
-                    >
-                      {breadcrumb.name}
-                    </a>
-                    <svg
-                      width={16}
-                      height={20}
-                      viewBox="0 0 16 20"
-                      fill="currentColor"
-                      aria-hidden="true"
-                      className="h-5 w-4 text-gray-300"
-                    >
-                      <path d="M5.697 4.34L8.98 16.532h1.327L7.025 4.341H5.697z" />
-                    </svg>
-                  </div>
-                </li>
-              ))}
               <li className="text-sm">
-                <a
-                  href={product.href}
+                <div
                   aria-current="page"
                   className="font-medium text-gray-500 hover:text-gray-600"
                 >
-                  {product.name}
-                </a>
+                  {productName}
+                </div>
               </li>
             </ol>
           </nav>
@@ -102,31 +90,31 @@ export const ProductLanding = () => {
           <div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
             <div className="aspect-h-4 aspect-w-3 hidden overflow-hidden rounded-lg lg:block">
               <img
-                src={product.images[0].src}
-                alt={product.images[0].alt}
+                src={thumbnail}
+                alt=""
                 className="h-full w-full object-cover object-center"
               />
             </div>
             <div className="hidden lg:grid lg:grid-cols-1 lg:gap-y-8">
               <div className="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
                 <img
-                  src={product.images[1].src}
-                  alt={product.images[1].alt}
+                  src={thumbnail}
+                  alt=""
                   className="h-full w-full object-cover object-center"
                 />
               </div>
               <div className="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
                 <img
-                  src={product.images[2].src}
-                  alt={product.images[2].alt}
+                  src={thumbnail}
+                  alt=""
                   className="h-full w-full object-cover object-center"
                 />
               </div>
             </div>
             <div className="aspect-h-5 aspect-w-4 lg:aspect-h-4 lg:aspect-w-3 sm:overflow-hidden sm:rounded-lg">
               <img
-                src={product.images[3].src}
-                alt={product.images[3].alt}
+                src={thumbnail}
+                alt=""
                 className="h-full w-full object-cover object-center"
               />
             </div>
@@ -136,16 +124,14 @@ export const ProductLanding = () => {
           <div className="mx-auto max-w-2xl px-4 pb-16 pt-10 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24 lg:pt-16">
             <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
               <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
-                {product.name}
+                {productName}
               </h1>
             </div>
 
             {/* Options */}
             <div className="mt-4 lg:row-span-3 lg:mt-0">
               <h2 className="sr-only">Product information</h2>
-              <p className="text-3xl tracking-tight text-gray-900">
-                {product.price}
-              </p>
+              <p className="text-3xl tracking-tight text-gray-900">{price}</p>
 
               {/* Reviews */}
               <div className="mt-6">
@@ -177,7 +163,7 @@ export const ProductLanding = () => {
 
               <form className="mt-10">
                 {/* Colors */}
-                <div>
+                {/* <div>
                   <h3 className="text-sm font-medium text-gray-900">Color</h3>
 
                   <RadioGroup
@@ -216,7 +202,7 @@ export const ProductLanding = () => {
                       ))}
                     </div>
                   </RadioGroup>
-                </div>
+                </div> */}
 
                 <button
                   type="submit"
@@ -246,11 +232,9 @@ export const ProductLanding = () => {
 
                 <div className="mt-4">
                   <ul role="list" className="list-disc space-y-2 pl-4 text-sm">
-                    {product.highlights.map((highlight) => (
-                      <li key={highlight} className="text-gray-400">
-                        <span className="text-gray-600">{highlight}</span>
-                      </li>
-                    ))}
+                    <li key={slug} className="text-gray-400">
+                      <span className="text-gray-600">{description}</span>
+                    </li>
                   </ul>
                 </div>
               </div>
@@ -259,7 +243,7 @@ export const ProductLanding = () => {
                 <h2 className="text-sm font-medium text-gray-900">Details</h2>
 
                 <div className="mt-4 space-y-6">
-                  <p className="text-sm text-gray-600">{product.details}</p>
+                  <p className="text-sm text-gray-600">{description}</p>
                 </div>
               </div>
             </div>
--- src/component/products/ProductsCard.jsx
+++ src/component/products/ProductsCard.jsx
@@ -3,65 +3,31 @@ import { Link } from "react-router-dom";
 
 export const ProductsCard = ({ filteredProduct }) => {
   return (
-    // <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-    //   <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-    //     <Link to={`/products/${slug}`}>
-    //       <div key={slug} className="p-1 border bg-black border-black rounded">
-    //         <img
-    //           src={thumbnail}
-    //           alt=""
-    //           class="h-64 w-full object-cover transition duration-500 group-hover:scale-105 sm:h-72"
-    //         />
-    //         <div className="relative border border-black bg-white p-6">
-    //           <span class="whitespace-nowrap bg-yellow-400 px-3 py-1.5 text-xs font-medium">
-    //             New
-    //           </span>
-
-    //           <h3 class="mt-4 text-lg font-medium text-gray-900">
-    //             {productName}
-    //           </h3>
-
-    //           <p class="mt-1.5 text-sm text-gray-700">{price}</p>
-
-    //           <form class="mt-4">
-    //             <button class="block w-full rounded bg-yellow-400 p-4 text-sm font-medium transition hover:scale-105">
-    //               Add to Cart
-    //             </button>
-    //           </form>
-    //         </div>
-    //       </div>
-    //     </Link>
-    //   </div>
-    // </div>
-
     <div className="mx-auto max-w-7xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
       <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-8">
         {filteredProduct.map((product) => (
           <div
             key={product.slug}
-            className="p-1 border bg-black border-black rounded"
+            className="bg-white p-4 border border-gray-200 rounded-lg shadow-md transition duration-300 transform hover:scale-105"
           >
             <Link to={`/products/${product.slug}`} className="block">
               <img
                 src={product.thumbnail}
-                alt=""
-                className="h-64 w-full object-cover transition duration-500 group-hover:scale-105 sm:h-72"
+                alt={product.productName}
+                className="h-64 w-full object-cover object-center"
               />
-              <div className="relative border border-black bg-white p-6">
-                <span className="whitespace-nowrap bg-yellow-400 px-3 py-1.5 text-xs font-medium">
-                  New
-                </span>
-                <h3 className="mt-4 text-lg font-medium text-gray-900">
+              <div className="mt-4">
+                <h3 className="text-lg font-semibold text-gray-900">
                   {product.productName}
                 </h3>
-                <p className="mt-1.5 text-sm text-gray-700">{product.price}</p>
-                <form className="mt-4">
-                  <button className="block w-full rounded bg-yellow-400 p-4 text-sm font-medium transition hover:scale-105">
-                    Add to Cart
-                  </button>
-                </form>
+                <p className="mt-2 text-sm text-gray-700">${product.price}</p>
               </div>
             </Link>
+            <form className="mt-4">
+              <button className="block w-full py-2 px-4 mt-2 rounded-md bg-green-500 text-white font-semibold shadow-md transition duration-300 transform hover:scale-105">
+                Add to Cart
+              </button>
+            </form>
           </div>
         ))}
       </div>
